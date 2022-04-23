efbc33cabeccd97fc3c4d8ccd83bfb11f5d2fc79
Suirabu <suirabu.dev@gmail.com>
2022-04-23 16:48:44 -0700

    Clear text formatting options at exit
    I encountered some issues which arose due to the program terminating
    before the graphical drawing routine had the chance to reset the
    terminal's text formatting options. This commit fixes this issue by
    creating a function which clears the text formatting options which will
    always be called at exit.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "board.h"
 #include "fen.h"
 
+static void clear_text_formatting(void) {
+    printf("\033[0m");  // Clear all text formatting options
+}
+
 int main(void) {
+    atexit(clear_text_formatting);
+
     Board board = fen_decode(FEN_STARTING_POSITION); 
     draw_board_graphical(board);
 
