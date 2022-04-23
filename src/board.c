5eb62173bb6502cb94271bf2fca44ad19bf213cc
Suirabu <suirabu.dev@gmail.com>
2022-04-23 02:10:49 -0700

    Refactor board drawing routine
    Refactored the board drawing routine to later support multiple different
    text representation of pieces.

--- src/board.c
+++ src/board.c
@@ -4,62 +4,54 @@
 #include "board.h"
 #include "error.h"
 
+static const char* ASCII_PIECES[] = {
+    ".",
+    "p", "n", "b", "r", "q", "k",
+    "P", "N", "B", "R", "Q", "K",
+};
+
+static const char* get_piece_str(Square sq, const char** pieces) {
+    switch(sq) {
+        case PAWN | BLACK:
+            return pieces[1];
+        case PAWN | WHITE:
+            return pieces[7];
+        case KNIGHT | BLACK:
+            return pieces[2];
+        case KNIGHT | WHITE:
+            return pieces[8];
+        case BISHOP | BLACK:
+            return pieces[3];
+        case BISHOP | WHITE:
+            return pieces[9];
+        case ROOK | BLACK:
+            return pieces[4];
+        case ROOK | WHITE:
+            return pieces[10];
+        case QUEEN | BLACK:
+            return pieces[5];
+        case QUEEN | WHITE:
+            return pieces[11];
+        case KING | BLACK:
+            return pieces[6];
+        case KING | WHITE:
+            return pieces[12];
+
+        case EMPTY:
+            return pieces[0];
+
+        default:
+            REPORT_ERROR("Invalid piece '0x%2X'\n", sq);
+            exit(EXIT_FAILURE);
+            break;
+    }
+}
+
 void draw_board(Board board) {
     for(int rank = 0; rank < 8; ++rank) {
         for(int file = 0; file < 8; ++file) {
             Square sq = board.squares[rank * 8 + file];
-            char c = '.';
-
-            switch(sq) {
-                case PAWN | BLACK:
-                    c = 'p';
-                    break;
-                case PAWN | WHITE:
-                    c = 'P';
-                    break;
-                case KNIGHT | BLACK:
-                    c = 'n';
-                    break;
-                case KNIGHT | WHITE:
-                    c = 'N';
-                    break;
-                case BISHOP | BLACK:
-                    c = 'b';
-                    break;
-                case BISHOP | WHITE:
-                    c = 'B';
-                    break;
-                case ROOK | BLACK:
-                    c = 'r';
-                    break;
-                case ROOK | WHITE:
-                    c = 'R';
-                    break;
-                case QUEEN | BLACK:
-                    c = 'q';
-                    break;
-                case QUEEN | WHITE:
-                    c = 'Q';
-                    break;
-                case KING | BLACK:
-                    c = 'k';
-                    break;
-                case KING | WHITE:
-                    c = 'K';
-                    break;
-
-                case EMPTY:
-                    c = '.';
-                    break;
-
-                default:
-                    REPORT_ERROR("Invalid piece '0x%2X' found on board\n", c);
-                    exit(EXIT_FAILURE);
-                    break;
-            }
-
-            putchar(c);
-            putchar(' ');
+            printf("%s ", get_piece_str(sq, ASCII_PIECES));
         }
 
         putchar('\n');
